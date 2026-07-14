9501d25ab9fc1fa80e3c583530e7766552e5515b
Emanuelly Gibson <manu.gibson14@gmail.com>
2026-07-13 21:48:51 -0300

    feat: implementar componente Produto e lista de itens

--- src/app/components/produto/produto.ts
+++ src/app/components/produto/produto.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
-
+import {UpperCasePipe} from '@angular/common';
 @Component({
   selector: 'app-produto',
-  imports: [],
+  imports: [UpperCasePipe],
   templateUrl: './produto.html',
   styleUrl: './produto.css',
 })
-export class Produto {}
+//foi adicionado variáveis e condicionais
+export class Produto {
+  produto = 'Notebook Gamer';
+  preco = 5000;
+  mostrarProduto = true;
+  mostrarPreco = true;
+produtos = [
+  {nome: 'Teclado', preco: 49.99},
+  {nome: 'Mouse', preco: 29.99},
+  {nome: 'Monitor', preco: 149.99}
+]
+}
