27274b78929f0d4574182d5f0593bdc5a6731bd3
Emanuelly Gibson <manu.gibson14@gmail.com>
2026-07-15 19:11:53 -0300

    refactor(app): substitui import Produto por ListaProduto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 //import { RouterOutlet } from '@angular/router';//remove a importação de RouterOutlet pois não é necessário para este componente
-import {Produto} from './components/produto/produto';
+//import {Produto} from './components/produto/produto';
+import { ListaProdutos } from './components/lista-produtos/lista-produtos';
 @Component({
   selector: 'app-root',
-  imports: [Produto],
+  imports: [ListaProdutos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
