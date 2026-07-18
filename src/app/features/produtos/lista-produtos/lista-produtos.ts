adb7b9f28c8a94d595150348a5b952c309f69bf5
Emanuelly Gibson Brito <manu.gibson14@gmail.com>
2026-07-17 21:30:14 -0300

    feat: adiciona imports, substituição de lista, monitoramento em tempo real e estado de seleção

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -3,24 +3,27 @@ import { Produto } from '../produto/produto';
 import  {signal} from "@angular/core";
 import {computed} from "@angular/core"
 import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
+import {effect} from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [Produto, PrecoFormatadoPipe],
+  imports: [Produto, PrecoFormatadoPipe, UpperCasePipe],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
   produtos = signal([
-    {nome: 'Teclado ' , preco:229.99},
-    {nome: 'Mouse ' , preco:129.99},
-    {nome: 'Monitor ' , preco:2000},
-    {nome: 'Desktop ' , preco:4999.99},
-    {nome: 'Headset ' , preco:500}
+    {nome: 'Teclado ' , preco:100},
+    {nome: 'Mouse ' , preco:50},
+    {nome: 'Monitor ' , preco:1500},
+    {nome: 'Desktop ' , preco:3000},
+    {nome: 'Headset ' , preco:150}
   ]);
   //!função para exibir produtos selecionados pelo usuário no console
   exibirProduto(nome: string){
     console.log('Produto Selecionado: ',nome);
+    this.produtoSelecionado.set(nome);
   }
   //função que adiciona produto usando metodo update
   adicionarProduto(){
@@ -36,4 +39,30 @@ export class ListaProdutos {
   {return this.produtos().reduce((total,
      item) =>
   total + item.preco,0)});
+     //!função que substitui a lista atual usando metodo set()
+     substituirProdutos(){
+      this.produtos.set([
+        {nome:'teclado Gamer', preco:350 },
+        {nome:'Mouse Gamer', preco: 150},
+        {nome:'Monitor Gamer',preco: 2000},
+        {nome:'Desktop Gamer',preco: 5000},
+        {nome:'Headset Gamer',preco: 650},
+      ]);
+     }
+     //! metodo para monitorar alterações em tempo real usando effect()
+     constructor(){
+      effect(() => {
+        console.log('Lista de Produtos Alterados: ', this.produtos());
+      });
+      effect(() => {
+        console.log('Valor Total Atualizado: ', this.valorTotal());
+      });
+      effect(() => {
+        if (typeof document !== 'undefined'){
+          document.title = `(${this.totalProdutos()}) - Loja da Emanuelly`;
+        }
+      });
+     }
+     //! metodo para criar um estado de seleção com signal string | null
+     produtoSelecionado = signal <string | null>(null);
 }
