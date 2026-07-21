0e95327b069fcb8c1af1155b68e6a8b37cac85f6
Emanuelly Gibson Brito <manu.gibson14@gmail.com>
2026-07-21 19:57:52 -0300

    refactor: remove listaProdutos, add Router e nome da loja

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,13 @@
 import { Component, signal } from '@angular/core';
-//import { RouterOutlet } from '@angular/router';//remove a importação de RouterOutlet pois não é necessário para este componente
-//import {Produto} from './components/produto/produto';
-import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
-
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { UpperCasePipe } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [ListaProdutos],
+  imports: [RouterOutlet, RouterLink, UpperCasePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
- protected readonly title = signal('e-commerce')
+ protected readonly title = signal('e-commerce');
+ nomeLoja = 'loja Da Manuzita';
 }
