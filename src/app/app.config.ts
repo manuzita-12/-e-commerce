016220fdb1c47c58bed98f245b3051ae36334813
Emanuelly Gibson Brito <manu.gibson14@gmail.com>
2026-08-03 21:10:55 -0300

    feat: adiciona configuração de interceptores no http client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,14 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { withInterceptors } from '@angular/common/http';
+import { HttpInterceptor } from './core/interceptors/http.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes), provideClientHydration(),
-    provideHttpClient(withFetch()),
-  ]
+    provideHttpClient(withFetch(),
+  withInterceptors([HttpInterceptor])),
+  ],
 };
