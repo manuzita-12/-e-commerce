e4e2597f7ac5e90ad56c6b0069862da174594aec
Emanuelly Gibson Brito <Manu.gibson14@gmail.com>
2026-08-12 21:04:15 -0300

    em checkout.ts: feat:implementa finalização de compra e validação de carrinho e em carrinho.service.ts: feat: adiciona propriedade para verificar carrinho vazio

--- src/app/core/services/carrinho.service.ts
+++ src/app/core/services/carrinho.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from "@angular/core";
 import { signal } from "@angular/core";
 import { computed } from "@angular/core";
+
+type ItemCarrinho = {
+    nome: string;
+    preco: number;
+}
+
 @Injectable ({ providedIn: 'root'})
 
 export class CarrinhoService{
 //Estado Global
-private carrinho = signal<{nome: string; preco: number}[]>([]); 
+private carrinho = signal<ItemCarrinho[]>([]); 
 
 //? Seleção
 itens = computed(() => this.carrinho());
 quantidadeItens = computed(() => this.carrinho().length);
 totalItens = computed(() => this.carrinho().reduce((total, item) => total + item.preco,0))
+carrinhoVazio = computed (() => this.carrinho().length === 0);
 
 // TODO: Ações: Adicionar Produtos
-adicionar(produto:{nome: string; preco: number}){
+adicionar(produto:ItemCarrinho){
     this.carrinho.update(lista =>[...lista, produto]);
 }
 // TODO: Limpeza
