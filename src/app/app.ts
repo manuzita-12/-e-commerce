c2a7d6f1c14b8e174fca22171294cd0f71071c7a
Emanuelly Gibson Brito <manu.gibson14@gmail.com>
2026-08-14 19:09:25 -0300

    refactor: limpa imports não utilizados

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { UpperCasePipe } from '@angular/common';
-import { usuarioLogado, login, logout } from './core/auth';
 import { MatButtonModule } from '@angular/material/button';
 import { Header } from './shared/layout/header/header';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, UpperCasePipe, MatButtonModule, Header],
+  imports: [RouterOutlet, MatButtonModule, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
  protected readonly title = signal('e-commerce');
- nomeLoja = 'loja Da Manuzita';
- usuarioLogado = usuarioLogado;
- login = login;
- logout = logout; 
 }
