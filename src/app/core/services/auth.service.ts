777b90c2aec4ee2c35a6854135461847070c394e
Emanuelly Gibson Brito <Manu.gibson14@gmail.com>
2026-08-13 21:50:35 -0300

    refactor: corrige tipo PerfilUsuario e atualiza método de login

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable, signal, computed } from "@angular/core";
 
+type PerfilUsuario = 'usuario';
+
 type Usuario ={
     email: string;
-    perfil: string;
+    perfil: PerfilUsuario;
 }
 
 @Injectable({
@@ -15,10 +17,27 @@ export class AuthService{
 
     //?COMPUTED
     usuarioAtual = computed(() => this.usuario());
-    usuarioLogado = computed(() => this.usuario() ! == null);
+    usuarioLogado = computed(() => this.usuario() !== null);
     token = computed(() => this.tokenJwt());
 
-    login(){}
+    login(email: string, senha: string): boolean {
+      
+        if(!email || !senha){
+            return false;
+        }
+        const tokenSimulado = 
+           'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' +
+           'eyJzdWIiOiJhbHVub0B0ZXN0ZS5jb20iLCJwZXJmaWwiOiJ1c3VhcmlvIn0.' 
+            +'assinatura-simulada';
+        this.usuario.set({
+            email,
+            perfil: 'usuario',
+        });
+        
+        this.tokenJwt.set(tokenSimulado);
+        return true;
+
+    }
 
     logout(){
         this.usuario.set(null);
