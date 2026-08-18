118cac80fece32fea7e8f6383890ec9825ccf31e
Emanuelly Gibson Brito <manu.gibson14@gmail.com>
2026-08-17 21:52:47 -0300

    feat: adiciona rota para acesso negado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -32,18 +32,27 @@ export const routes: Routes = [
         path:'checkout',
         canActivate: [authGuard],
         loadComponent: () => 
-            import('./features/checkout/checkout/checkout').then((m)=> m.Checkout),
+            import('./features/checkout/checkout/checkout')
+        .then((m)=> m.Checkout),
     },
     {
         path: 'login',
         loadComponent: () =>
-            import('./features/login/login/login').then((m) => m.Login),
+            import('./features/login/login/login')
+        .then((m) => m.Login),
     },
     {
         path:'admin',
         canActivate: [adminGuard],
         loadComponent: () =>
-            import('./features/admin/admin/admin').then((m) => m.Admin),
+            import('./features/admin/admin/admin')
+        .then((m) => m.Admin),
+    },
+    {
+        path: 'acesso-negado',
+        loadComponent: () =>
+            import('./features/acesso-negado/acesso-negado/acesso-negado')
+        .then((m) => m.AcessoNegado),
     },
     {
         path:'**',
