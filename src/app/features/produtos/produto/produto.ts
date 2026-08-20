81c7efafd8d8ade6809ba387df96c8edb1d06b40
Emanuelly Gibson Brito <manu.gibson14@gmail.com>
2026-08-20 19:18:56 -0300

    feat:altera output em emitir o objeto ItemCArrinho

--- src/app/features/produtos/produto/produto.ts
+++ src/app/features/produtos/produto/produto.ts
@@ -3,6 +3,7 @@ import {UpperCasePipe,  CurrencyPipe} from '@angular/common';
 import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 import { MatButtonModule} from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { ItemCarrinho } from '../../../core/models/item-carrinho';
 
 @Component({
   selector: 'app-produto',
@@ -20,10 +21,7 @@ export class Produto {
 selecionarProduto(){
   this.produtoSelecionado.emit(this.nome);
  }
- @Output() produtoAdicionado = new EventEmitter<{
-  nome: string;
-  preco: number;
- }>();
+ @Output() produtoAdicionado = new EventEmitter<ItemCarrinho>();
  adicionarAoCarrinho(){
   this.produtoAdicionado.emit({
 nome:this.nome,
