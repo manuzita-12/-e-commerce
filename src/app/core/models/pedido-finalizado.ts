000ba873a0a1351c9402f6a4eeb8785d25b95b71
Emanuelly Gibson Brito <manu.gibson14@gmail.com>
2026-08-21 20:41:53 -0300

    feat: cria modelo de pedido finalizado para o checkout

---
+++ src/app/core/models/pedido-finalizado.ts
@@ -0,0 +1,10 @@
+import { ItemCarrinho } from "./item-carrinho";
+
+export type PedidoFinalizado = {
+    codigo: number;
+    cliente: string;
+    quantidadeItens: number;
+    total: number;
+    itens: ItemCarrinho[];
+    email: string
+};
